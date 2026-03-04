0fff8093fa559912f11a1c292e5586c1b5aee242
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-03-04 21:53:11 +0000

    Wrap layout in AuthProvider for improved authentication context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from '../hooks/use-color-scheme';
+import { AuthProvider } from '../context/AuthContext';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,17 +14,19 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="login" />
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="abonnement" options={{ presentation: 'modal' }} />
-        <Stack.Screen name="voyage_control" />
-        <Stack.Screen name="renew" options={{ presentation: 'modal' }} />
-        <Stack.Screen name="pass" options={{ presentation: 'modal' }} />
-        <Stack.Screen name="notifications" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="login" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="abonnement" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="voyage_control" />
+          <Stack.Screen name="renew" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="pass" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="notifications" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
